322167649bf21f6c57e611a97d982d572668e4c9
Nicolas Béreux <nicolas.bereux@gmail.com>
2023-02-05 11:45:26 +0100

    update

---
+++ lcc/models/segformer_encoder.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+from torchvision.ops import StochasticDepth
+from torch import Tensor
+from typing import List, Iterable
+from einops import rearrange
+
+
+class LayerNorm2d(nn.LayerNorm):
+
+    def forward(self, input: Tensor) -> Tensor:
+        x = rearrange(input, 'b c h w -> b h w c')
+        x = super().forward(x)
+        x = rearrange(x, 'b h w c -> b c h w')
+        return x
+
+class OverlapPatchMerging(nn.Sequential):
+    def __init__(self, in_channels, out_channels, patch_size, overlap_size):
+        super().__init__(
+            nn.Conv2d(
+                in_channels,
+                out_channels, 
+                kernel_size=patch_size, 
+                stride=overlap_size, 
+                padding=patch_size//2, 
+                bias=False),
+            LayerNorm2d(out_channels)
+        )
+
+class EfficientMultiHeadAttention(nn.Module):
+    def __init__(self, channels: int, reduction_ratio: int = 1, num_heads: int = 8):
+        super().__init__()
+        self.reducer = nn.Sequential(
+            nn.Conv2d(
+                channels, channels, kernel_size=reduction_ratio, stride=reduction_ratio
+            ),
+            LayerNorm2d(channels),
+        )
+        self.att = nn.MultiheadAttention(
+            channels, num_heads=num_heads, batch_first=True
+        )
+
+    def forward(self, x):
+        _, _, h, w = x.shape
+        reduced_x = self.reducer(x)
+        # attention needs tensor of shape (batch, sequence_length, channels)
+        reduced_x = rearrange(reduced_x, "b c h w -> b (h w) c")
+        x = rearrange(x, "b c h w -> b (h w) c")
+        out = self.att(x, reduced_x, reduced_x)[0]
+        # reshape it back to (batch, channels, height, width)
+        out = rearrange(out, "b (h w) c -> b c h w", h=h, w=w)
+        return out
+
+class MixMLP(nn.Sequential):
+    def __init__(self, channels: int, expansion: int = 4):
+        super().__init__(
+            # dense layer
+            nn.Conv2d(channels, channels, kernel_size=1),
+            # depth wise conv
+            nn.Conv2d(
+                channels,
+                channels * expansion,
+                kernel_size=3,
+                groups=channels,
+                padding=1,
+            ),
+            nn.GELU(),
+            # dense layer
+            nn.Conv2d(channels * expansion, channels, kernel_size=1),
+        )
+
+
+class ResidualAdd(nn.Module):
+    """Just an util layer"""
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        out = self.fn(x, **kwargs)
+        x = x + out
+        return x
+
+class SegFormerEncoderBlock(nn.Sequential):
+    def __init__(
+        self,
+        channels: int,
+        reduction_ratio: int = 1,
+        num_heads: int = 8,
+        mlp_expansion: int = 4,
+        drop_path_prob: float = .0
+    ):
+        super().__init__(
+            ResidualAdd(
+                nn.Sequential(
+                    LayerNorm2d(channels),
+                    EfficientMultiHeadAttention(channels, reduction_ratio, num_heads),
+                )
+            ),
+            ResidualAdd(
+                nn.Sequential(
+                    LayerNorm2d(channels),
+                    MixMLP(channels, expansion=mlp_expansion),
+                    StochasticDepth(p=drop_path_prob, mode="batch")
+                )
+            ),
+        )
+
+class SegFormerEncoderStage(nn.Sequential):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        patch_size: int,
+        overlap_size: int,
+        drop_probs: List[int],
+        depth: int = 2,
+        reduction_ratio: int = 1,
+        num_heads: int = 8,
+        mlp_expansion: int = 4,
+    ):
+        super().__init__()
+        self.overlap_patch_merge = OverlapPatchMerging(
+            in_channels, out_channels, patch_size, overlap_size,
+        )
+        self.blocks = nn.Sequential(
+            *[
+                SegFormerEncoderBlock(
+                    out_channels, reduction_ratio, num_heads, mlp_expansion, drop_probs[i]
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = LayerNorm2d(out_channels)
+
+
+def chunks(data: Iterable, sizes: List[int]):
+    """
+    Given an iterable, returns slices using sizes as indices
+    """
+    curr = 0
+    for size in sizes:
+        chunk = data[curr: curr + size]
+        curr += size
+        yield chunk
+        
+class SegFormerEncoder(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        widths: List[int],
+        depths: List[int],
+        all_num_heads: List[int],
+        patch_sizes: List[int],
+        overlap_sizes: List[int],
+        reduction_ratios: List[int],
+        mlp_expansions: List[int],
+        drop_prob: float = .0
+    ):
+        super().__init__()
+        # create drop paths probabilities (one for each stage's block)
+        drop_probs =  [x.item() for x in torch.linspace(0, drop_prob, sum(depths))]
+        self.stages = nn.ModuleList(
+            [
+                SegFormerEncoderStage(*args)
+                for args in zip(
+                    [in_channels, *widths],
+                    widths,
+                    patch_sizes,
+                    overlap_sizes,
+                    chunks(drop_probs, sizes=depths),
+                    depths,
+                    reduction_ratios,
+                    all_num_heads,
+                    mlp_expansions
+                )
+            ]
+        )
+        
+    def forward(self, x):
+        features = []
+        for stage in self.stages:
+            x = stage(x)
+            features.append(x)
+        return features
+
